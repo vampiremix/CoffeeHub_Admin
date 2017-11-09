73202e909e10a76f117718683e8f5d0ccbbb3528
vampiremix <nutprapobsun.mick@gmail.com>
2017-11-09 11:09:19 +0700

    mick edit root page set to login page

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,7 +1,7 @@
 import { UserComponent } from './user/user.component';
 import { NgModule } from '@angular/core';
 import { CommonModule, } from '@angular/common';
-import { BrowserModule  } from '@angular/platform-browser';
+import { BrowserModule } from '@angular/platform-browser';
 import { Routes, RouterModule } from '@angular/router';
 
 import { DashboardComponent } from './dashboard/dashboard.component';
@@ -13,19 +13,21 @@ import { MapsComponent } from './maps/maps.component';
 import { NotificationsComponent } from './notifications/notifications.component';
 import { UpgradeComponent } from './upgrade/upgrade.component';
 import { ShopListComponent } from 'app/shop-list/shop-list.component';
+import { LoginComponent } from 'app/login/login.component';
 
-const routes: Routes =[
-    { path: 'dashboard',      component: DashboardComponent },
-    { path: 'shop-list',      component: ShopListComponent },
-    { path: 'user',       component: UserComponent },
-    { path: 'user-profile',   component: UserProfileComponent },
-    { path: 'table-list',     component: TableListComponent },
-    { path: 'typography',     component: TypographyComponent },
-    { path: 'icons',          component: IconsComponent },
-    { path: 'maps',           component: MapsComponent },
-    { path: 'notifications',  component: NotificationsComponent },
-    { path: 'upgrade',        component: UpgradeComponent },
-    { path: '',               redirectTo: 'dashboard', pathMatch: 'full' }
+const routes: Routes = [
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'shop-list', component: ShopListComponent },
+  { path: 'user', component: UserComponent },
+  { path: 'user-profile', component: UserProfileComponent },
+  { path: 'table-list', component: TableListComponent },
+  { path: 'typography', component: TypographyComponent },
+  { path: 'icons', component: IconsComponent },
+  { path: 'maps', component: MapsComponent },
+  { path: 'notifications', component: NotificationsComponent },
+  { path: 'upgrade', component: UpgradeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: '', redirectTo: 'login', pathMatch: 'full' }
 ];
 
 @NgModule({
