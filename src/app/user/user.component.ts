7d6b5a7971b09a87fe6f822b7229318bac40c4aa
vampiremix <nutprapobsun.mick@gmail.com>
2017-11-09 10:00:35 +0700

    Mick update user module

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Http, Response, RequestOptions, Headers } from '@angular/http';
+
 
 @Component({
   selector: 'app-user',
@@ -6,10 +8,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user.component.css']
 })
 export class UserComponent implements OnInit {
+  headers = new Headers({
+    'Content-Type': 'application/json'
+  });
 
-  constructor() { }
+  optionsURL = new RequestOptions({
+    headers: this.headers
+  });
+  constructor(private http: Http) { }
 
   ngOnInit() {
+    let signindata = { username: "amonratCha", password: "P@ssw0rd1234"};
+    this.http.post('https://coffeehubserver.herokuapp.com/api/auth/signin', signindata, this.optionsURL).subscribe(data => {
+      console.log("user : " , data);
+      let ss = {name:"test"};
+      this.http.post('https://coffeehubserver.herokuapp.com/api/favorites/',ss,this.optionsURL).subscribe(data2 => {
+        console.log("Shop response : ", data2);
+      })
+    });
+   
   }
 
 }
