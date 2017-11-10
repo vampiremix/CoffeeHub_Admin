61ee902c0534c919cab125c946c64773d14bf530
amonratCha <amonrat.cha@northbkk.ac.th>
2017-11-10 17:32:18 +0700

    register shopowner

--- src/app/create-shopowner/create-shopowner.component.ts
+++ src/app/create-shopowner/create-shopowner.component.ts
@@ -1,3 +1,7 @@
+import { RouteService } from '../route.service';
+import { Http } from '@angular/http';
+import { UsersModel } from '../user/user.model';
+import { FormControl, FormGroup } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +11,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateShopownerComponent implements OnInit {
 
-  constructor() { }
+  login: FormGroup;
+  public edit1: UsersModel = new UsersModel();
+
+  constructor(private http: Http, private route: RouteService) {
+    
+    this.login = new FormGroup({
+      
+      username: new FormControl(''),
+      password: new FormControl(''),
+      confirm_password: new FormControl(''),
+      firstName: new FormControl(''),
+      lastName: new FormControl(''),
+      email: new FormControl(''),
+      phone: new FormControl('')
+
+    });
+   }
 
   ngOnInit() {
+  }
+
+  postDataShopOwner(){
+    let token =  this.route.createAuthorizationHeader();
+
+    this.edit1.username = this.login.value.username;
+    this.edit1.password = this.login.value.password;
+    this.edit1.firstName = this.login.value.firstName;
+    this.edit1.lastName = this.login.value.lastName;
+    this.edit1.email = this.login.value.email;
+    this.edit1.phone = this.login.value.phone;
+
+    alert(JSON.stringify(this.edit1));
+
+    this.http.post(this.route.route + 'api/auth/signup/', this.edit1).toPromise().then((res) => {
+     console.log(res);
+    }).catch((err) => {
+      console.log("Cannot get shop list :", err);
+    });
   }
 
 }
