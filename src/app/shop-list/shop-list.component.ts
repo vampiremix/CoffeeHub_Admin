0d40da90c9583c38592da2fa178919f57d446f3c
vampiremix <nutprapobsun.mick@gmail.com>
2017-11-14 16:13:04 +0700

    mick update shop-list

--- src/app/shop-list/shop-list.component.ts
+++ src/app/shop-list/shop-list.component.ts
@@ -14,20 +14,19 @@ import { FormGroup, FormControl } from '@angular/forms';
 
 export class ShopListComponent implements OnInit {
   public shoplist: Array<ShopsModel> = new Array<ShopsModel>();
-  public editing: Array<ShopsModel> = new Array<ShopsModel>();
+  public editing: ShopsModel = new ShopsModel();
   public isEdit: Boolean = false;
   public isAdd: Boolean = false;
   public shopcode;
   public edittitle;
   public showSON = false;
-  public shopowner;
+  public getshopowner;
   addShop: FormGroup;
   // public addData: any;
 
   selectedCar: string;
   parkingOpt;
   //cars: SelectItem[];
-
   public sendAddShopData: ShopsModel = new ShopsModel();
   constructor(private http: Http, private route: RouteService) {
     this.addShopStructure();
@@ -60,8 +59,6 @@ export class ShopListComponent implements OnInit {
       shopowner: new FormControl('')
     })
   }
-
-
   ngOnInit() {
     this.getShoplist();
     this.getShopowner();
@@ -80,8 +77,8 @@ export class ShopListComponent implements OnInit {
   getShopowner() {
     let token = this.route.createAuthorizationHeader();
     this.http.get(this.route.route + 'api/users/shopowner', { headers: token }).toPromise().then((res) => {
-      this.shopowner = res.json();
-      console.log("Shop Owner ", this.shopowner);
+      this.getshopowner = res.json();
+      console.log("Shop Owner ", this.getshopowner);
     }).catch((err) => {
       console.log("Cannot get shop list :", err);
 
@@ -115,7 +112,7 @@ export class ShopListComponent implements OnInit {
     this.sendAddShopData.instagram = this.addShop.value.instagram;
     this.sendAddShopData.line = this.addShop.value.line;
     this.sendAddShopData.shopowner = this.addShop.value.shopowner;
-    // this.sendAddShopData.parking = this.addShop.value.parking;
+    this.sendAddShopData.parking = this.addShop.value.parking;
     this.sendAddShopData.createduser = user._id;
     console.log("ADD SHOP : ", this.sendAddShopData);
     if (this.sendAddShopData.name !== null && this.sendAddShopData.address.address !== null) {
@@ -153,16 +150,22 @@ export class ShopListComponent implements OnInit {
     if (this.isEdit == true) {
       this.editing = null;
     }
+    this.getShopowner();
     this.isEdit = true;
     this.isAdd = false;
     this.edittitle = shop.name;
     this.shopcode = shop.shopcode;
     console.log("SHOP : ", shop);
     this.editing = JSON.parse(JSON.stringify(shop));
+    this.editing.openinghours.open = this.editing.openinghours.open ? new Date(this.editing.openinghours.open) : new Date(2017,1,1,0,0,0);
+    this.editing.openinghours.close = this.editing.openinghours.close ? new Date(this.editing.openinghours.close) : new Date(2017,1,1,0,0,0);
+    console.log("TIME : " , this.editing[0].openinghours.open);
   }
   updateData(data) {
     let token = this.route.createAuthorizationHeader();
     let user = JSON.parse(window.localStorage.getItem("user"));
+    data.openinghours.open = new Date(Date.parse(data.openinghours.open));
+    data.openinghours.close = new Date(Date.parse(data.openinghours.close));
     data.editlog.user = user._id;
     this.http.put(this.route.route + 'api/shops/' + data._id, data, { headers: token }).toPromise().then((res) => {
       // this.shoplist = res.json();
