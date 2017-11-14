f63d3a7c7882a51e769119aab509591b7262b9a6
vampiremix <nutprapobsun.mick@gmail.com>
2017-11-14 16:44:54 +0700

    mick add notification

--- src/app/notifications/notifications.component.ts
+++ src/app/notifications/notifications.component.ts
@@ -1,24 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Injectable } from '@angular/core';
 declare var $: any;
 @Component({
   selector: 'app-notifications',
   templateUrl: './notifications.component.html',
   styleUrls: ['./notifications.component.css']
 })
+@Injectable()
 export class NotificationsComponent implements OnInit {
 
   constructor() { }
-  showNotification(from, align){
+  showNotification(from, align,text,types){
       const type = ['','info','success','warning','danger'];
 
-      const color = Math.floor((Math.random() * 4) + 1);
+    //   const color = Math.floor((Math.random() * 4) + 1);
 
       $.notify({
           icon: "notifications",
-          message: "Welcome to <b>Material Dashboard</b> - a beautiful freebie for every web developer."
+          message: text
 
       },{
-          type: type[color],
+          type: type[types],
           timer: 4000,
           placement: {
               from: from,
